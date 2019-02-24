4774c44a71f4330a0cc8dd76fb94aad7d7a0d2d2
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-02-24 18:05:34 -0500

    put article click listener in function to run after articles loaded, still, the clicks only work for articles with notes attached to them for some reason

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,4 @@
-// Note: this is copied directly from class activity in order to get full app running.  Will be customized later when page functionality is changed.
+
 $(document).ready(function() {
   function getArticles() {
     $.ajax({
@@ -11,57 +11,63 @@ $(document).ready(function() {
           .append(article.title)
           .attr('data-id', article._id)
           .appendTo($('#articles'));
+          submitListener();
       });
     });
   }
 
-  $('#articles').on('click', '.article', function() {
+  function submitListener() {
+    $('#articles').on('click', '.article', function() {
 
-    const articleId = $(this).attr('data-id');
-    $('#note-title').val('');
-    $('#note-body').val('');
-    
-    $.ajax({
-      url: `/articles/${articleId}`,
-      method: 'GET'
-    }).then(function(articleData) {
-      console.log(articleData);
-      $('#submit-note').attr('data-id', articleData._id).attr("data-note-id", articleData.note._id);
-      $('#article-link')
-        .attr('href', 'https://aeon.co'+articleData.link)
-        .text(articleData.title);
-      $('#note-body').val(articleData.note.body);
-      $('#note-title').val(articleData.note.title);
-    });
-  });
-
-  $('#submit-note').on('click', function(e) {
-    e.preventDefault();
-
-    const articleId = $(this).attr('data-id');
-    if (!articleId) {
-      return false;
-    }
-
-    const noteData = {
-      title: $('#note-title')
-        .val()
-        .trim(),
-      body: $('#note-body')
-        .val()
-        .trim()
-    };
-
-    $.ajax({
-      url: `/articles/${articleId}`,
-      method: 'POST',
-      data: noteData
-    }).then(function(data) {
-      console.log(data);
+      const articleId = $(this).attr('data-id');
       $('#note-title').val('');
       $('#note-body').val('');
+      
+      $.ajax({
+        url: `/articles/${articleId}`,
+        method: 'GET'
+      }).then(function(articleData) {
+        console.log(articleData);
+        $('#submit-note').attr('data-id', articleData._id).attr("data-note-id", articleData.note._id);
+        $('#article-link')
+          .attr('href', 'https://aeon.co'+articleData.link)
+          .text(articleData.title);
+        $('#note-body').val(articleData.note.body);
+        $('#note-title').val(articleData.note.title);
+        
+      });
     });
-  });
+  };
+
+  
+    $('#submit-note').on('click', function(e) {
+      e.preventDefault();
+
+      const articleId = $(this).attr('data-id');
+      if (!articleId) {
+        return false;
+      }
+
+      const noteData = {
+        title: $('#note-title')
+          .val()
+          .trim(),
+        body: $('#note-body')
+          .val()
+          .trim()
+      };
+
+      $.ajax({
+        url: `/articles/${articleId}`,
+        method: 'POST',
+        data: noteData
+      }).then(function(data) {
+        console.log(data);
+        $('#note-title').val('');
+        $('#note-body').val('');
+      });
+    });
+
 
   getArticles();
 });
